9e58fea110c398431cbb33f9e1427f0ce1a402f1
stdrill <tanya23.96@mail.ru>
2022-09-15 22:30:36 +0300

    task4 added

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+void PrintArray (int[,,] matr)
+{
+    for(int i = 0;i < matr.GetLength(0); i++)
+    {
+        for(int j = 0;j < matr.GetLength(1); j++)
+        {
+            for(int z = 0;z < matr.GetLength(2);z++)
+            {
+                Console.Write($"{matr[i,j,z]} ");
+                
+            }
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,,] matr)
+{
+ for(int i = 0;i < matr.GetLength(0); i++)
+    {
+        for(int j = 0;j < matr.GetLength(1); j++)
+        {
+            for(int z = 0;z < matr.GetLength(2);z++)
+            {
+                matr[i, j, z] = new Random().Next(10,100);
+            }
+        }
+    }   
+}
+
+int[,,] matrix = new int [2,2,2];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
---
+++ task4/Program.cs
@@ -0,0 +1,45 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+
+int row = 9;
+int[,] arrayTr = new int[row,row];
+const int cellWidth = 3;
+
+void FillTriangle(int [,] triangle)
+{
+
+    for (int i = 0; i < row; i++)
+    {
+        triangle[i,0] = 1;
+        triangle[i,i] = 1;
+    }
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i,j] = 
+                triangle[i-1,j-1] + triangle[i-1, j];
+        }
+    }
+}
+
+void PrintTriangle(int [,] triangle)
+{   
+    int col = cellWidth * row;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if(triangle[i,j] != 0) Console.Write($"{triangle[i,j],cellWidth}");
+            col = col + cellWidth * 2;
+        }
+        col = cellWidth * row - cellWidth * (i + 1);
+        Console.WriteLine();
+    }
+}
+
+
+
+FillTriangle(arrayTr);
+PrintTriangle(arrayTr);
